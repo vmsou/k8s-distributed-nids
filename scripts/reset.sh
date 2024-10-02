1bdc1419b19eaa12d53bf7b70b551be4843fe963
vmsou <viniciusmarcs@icloud.com>
2024-10-02 14:56:09 -0300

    update: Scripts - reset.sh (user home when sudoing)

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,6 +1,7 @@
 sudo kubeadm reset
 
-rm -rf "$HOME"/.kube/
+USER_HOME=$(eval echo ~${SUDO_USER})
+rm -rf "$USER_HOME"/.kube/
 sudo rm -rf /etc/kubernetes/
 sudo rm -rf /var/lib/kubelet/
 sudo rm -rf /var/lib/etcd
