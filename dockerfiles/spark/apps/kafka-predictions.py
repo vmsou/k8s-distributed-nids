7d60ae63581f889b0bfac6abfd4b7d9effcc245f
vmsou <viniciusmarcs@icloud.com>
2024-10-15 19:03:52 -0300

    feat: Spark Apps - kafka-prediction.py (batch ev/s)

--- dockerfiles/spark/apps/kafka-predictions.py
+++ dockerfiles/spark/apps/kafka-predictions.py
@@ -23,9 +23,6 @@ def parse_arguments():
     parser.add_argument("--trigger", help="Type of trigger (micro-batch, interval, available-now)", choices=["micro-batch", "interval", "available-now"], default="micro-batch")
     parser.add_argument("--trigger-interval", help="Trigger interval time (e.g., '1 second', '10 seconds', '1 minute')", default="1 second")
 
-    parser.add_argument("--parallelism", help="Parallelism's Number (defaults to sparkContext.defaultParallelism)", type=int, default=None)
-    parser.add_argument("--partitions", help="Dataset repartitions (defaults to paralellism * 2)", required=False, default=None, type=int)
-
     return parser.parse_args()
 
 
@@ -61,8 +58,6 @@ def main() -> None:
     TRIGGER_INTERVAL: str = args.trigger_interval
     SCHEMA_PATH: str = args.schema
     FORMAT: str = args.format
-    PARTITIONS = args.partitions
-    PARALLELISM = args.parallelism
 
     print()
     print(" [CONF] ".center(50, "-"))
@@ -74,21 +69,11 @@ def main() -> None:
         print(f"{TRIGGER_INTERVAL=}")
     print(f"{SCHEMA_PATH=}")
     print(f"{FORMAT=}")
-    print(f"{PARTITIONS=}")
-    print(f"{PARALLELISM=}")
     print()
 
     spark = create_session()
     sc: SparkContext = spark.sparkContext
     sc.setLogLevel("WARN")
-
-    if PARALLELISM is None:
-        PARALLELISM = spark.sparkContext.defaultParallelism
-        print(f"Parallelism is set to None. Defaulting to {PARALLELISM}")
-
-    if PARTITIONS is None:
-        PARTITIONS = PARALLELISM * 2
-        print(f"Partition is set to None. Defaulting to {PARTITIONS}")
 
     print()
     print(" [MODEL] ".center(50, "-"))
@@ -117,21 +102,29 @@ def main() -> None:
     conditions = get_conditions_from_schema(schema)
 
     # for field in schema.jsonValue()["fields"]:
-        # print(f"{field['name']}: {field['type']}")
+    #   print(f"{field['name']}: {field['type']}")
     print(schema.simpleString())
 
     print(f"OK. Loaded in {t1 - t0}s")
     print()
 
     print(" [KAFKA] ".center(50, "-"))
+    kafka_params = {
+        "kafka.bootstrap.servers": BROKERS,
+        "subscribe": TOPIC,
+        "startingOffsets": "latest",
+        #"max.partition.fetch.bytes": "52428800",  # 50 MB
+        #"fetch.min.bytes": "1048576",  # 1 MB
+        #"fetch.max.wait.ms": "100",    # 100 ms
+        #"consumer.poll.timeout.ms": "500"
+    }   
     print("Setting stream...")
-    df = spark.readStream \
+    print(kafka_params)
+    stream_df = spark.readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", BROKERS) \
-        .option("subscribe", TOPIC) \
-        .option("startingOffsets", "latest") \
+        .options(**kafka_params) \
         .load()
-    print("OK")
+    print(f"OK.")
 
     # .selectExpr(f"from_csv(value, '{schema.simpleString()}') AS features")
     format_func = None
@@ -139,32 +132,31 @@ def main() -> None:
     elif FORMAT == "json": format_func = F.from_json("value", schema)
     else: raise Exception(f"Format not supported: {FORMAT}.")
 
-    features = df \
-        .selectExpr("CAST(value AS STRING)") \
-        .select(format_func.alias("features")) \
-        .select("features.*")
-    
-    valid_features = features.filter(F.expr(" AND ".join([c._jc.toString() for c in conditions])))
+    stream_df = stream_df.selectExpr("CAST(value AS STRING)")
+    parsed_df = stream_df.select(format_func.alias("features")).select("features.*").filter(F.expr(" AND ".join([c._jc.toString() for c in conditions])))
 
+    # stream_timestamp_df = parsed_df.withColumn("processing_time", F.current_timestamp())
+    
     print(" [PREDICTIONS] ".center(50, "-"))
+    predictions = model.transform(parsed_df)
+
     def process_batch(batch_df, batch_id):
-        print(f"Processing batch {batch_id}...")
-        batch_df = batch_df.repartition(PARTITIONS)
         t0 = time.time()
-        predictions = model.transform(batch_df) #.select(features_col, prediction_col, "probability")
-        num_rows = predictions.count() # Trigger computations
+        event_count = batch_df.count()
         t1 = time.time()
-        batch_duration = t1 - t0
-        if batch_duration > 0:
-            ev_per_sec = num_rows / batch_duration
-            print(f"Batch {batch_id}: Processed {num_rows} rows in {batch_duration:.2f} seconds ({ev_per_sec:.2f} EV/S)")
+        time_taken = t1 - t0
+        if time_taken > 0:
+            eps = event_count / time_taken
+            print(f"Batch {batch_id}: {eps:.2f} EV/S - {event_count} events in {time_taken:.2f} seconds")
+        else:
+            print(f"Batch {batch_id}: No time taken for batch processing (likely empty batch).")
     
     # Default micro-batch
-    query = valid_features.writeStream.foreachBatch(process_batch)
+    query = predictions.writeStream.foreachBatch(process_batch).outputMode("update")
 
     if TRIGGER_TYPE == "interval": query = query.trigger(processingTime=TRIGGER_INTERVAL)
     elif TRIGGER_TYPE == "available-now": query = query.trigger(availableNow=True)
-
+    
     query.start().awaitTermination()
 
 if __name__ == "__main__":
