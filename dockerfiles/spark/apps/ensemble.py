51b1ebb8e42e223154543d74ebb348657aedfdd9
vmsou <viniciusmarcs@icloud.com>
2024-11-21 19:35:28 -0300

    feat: Spark Apps - ensemble.py (Multiclass support & predictionsCol)

--- dockerfiles/spark/apps/ensemble.py
+++ dockerfiles/spark/apps/ensemble.py
@@ -9,14 +9,15 @@ import pyspark.sql.functions as F
 
 class EnsembleClassifier(Transformer, DefaultParamsWritable, DefaultParamsReadable):
     # string parameters as Spark Params
-    predictionCol = Param(Params._dummy(), "predictionCol", "Column name for predictions")
+    predictionCol = Param(Params._dummy(), "predictionCol", "Column name for ensemble prediction")
+    predictionsCol = Param(Params._dummy(), "predictionsCol", "Column name for predictions array")
     rawPredictionCol = Param(Params._dummy(), "rawPredictionCol", "Column name for raw predictions")
     probabilityCol = Param(Params._dummy(), "probabilityCol", "Column name for probabilities")
     mode = Param(Params._dummy(), "mode", "Voting mode: majority, attack, or normal")
 
-    def __init__(self, models: List[Transformer] = None, predictionCol: str = "ensemble_prediction", rawPredictionCol: str = "rawPrediction", probabilityCol: str = "probability",mode: Literal["majority", "attack", "normal"] = "majority"):
+    def __init__(self, models: List[Transformer] = None, predictionCol: str = "ensemble_prediction", predictionsCol: str = "predictions", rawPredictionCol: str = "rawPrediction", probabilityCol: str = "probability",mode: Literal["majority", "attack", "normal"] = "majority"):
         super(EnsembleClassifier, self).__init__()
-        self._setDefault(predictionCol=predictionCol, rawPredictionCol=rawPredictionCol, probabilityCol=probabilityCol, mode=mode)
+        self._setDefault(predictionCol=predictionCol, predictionsCol=predictionsCol, rawPredictionCol=rawPredictionCol, probabilityCol=probabilityCol, mode=mode)
 
         self.models = models if models else []
 
@@ -27,6 +28,7 @@ class EnsembleClassifier(Transformer, DefaultParamsWritable, DefaultParamsReadab
 
     def getMode(self): return self.getOrDefault("mode")
     def getPredictionCol(self): return self.getOrDefault(self.predictionCol)
+    def getPredictionsCol(self): return self.getOrDefault(self.predictionsCol)
     def getRawPredictionCol(self): return self.getOrDefault(self.rawPredictionCol)
     def getProbabilityCol(self): return self.getOrDefault(self.probabilityCol)
     def getLabelCol(self): return self.labelCol
@@ -34,8 +36,9 @@ class EnsembleClassifier(Transformer, DefaultParamsWritable, DefaultParamsReadab
     def _transform(self, df: DataFrame) -> DataFrame:
         if not self.models: raise ValueError("No models provided for ensemble voting.")
 
-        predictions_col = "predictions"
+        predictions_col = self.getPredictionsCol()
         relevant_columns = df.columns + [predictions_col]
+        num_classes = self.models[0].stages[-1].numClasses
 
         df = df.withColumn(predictions_col, F.array())
         for model in self.models:
@@ -48,8 +51,9 @@ class EnsembleClassifier(Transformer, DefaultParamsWritable, DefaultParamsReadab
         # Computer rawPrediction: count of each class
         df = df.withColumn(
             self.getRawPredictionCol(),
-            F.expr(f"aggregate({predictions_col}, array(0.0d, 0.0d), (acc, x) -> array(acc[0] + (1 - x), acc[1] + x))")
+            F.expr(f"aggregate({predictions_col}, array_repeat(0.0d, {num_classes}), (acc, x) -> transform(acc, (v, i) -> if (i = x, v + 1, v)))")
         )
+
 
         # Compute probabilities: count percentage to quantity of models
         df = df.withColumn(
@@ -59,9 +63,8 @@ class EnsembleClassifier(Transformer, DefaultParamsWritable, DefaultParamsReadab
 
         mode = self.getOrDefault("mode")
         if mode == "majority": # Majority voting
-            sum_predictions = F.expr(f"aggregate({predictions_col}, 0.0d, (acc, x) -> acc + x)")
-            majority_threshold = len(self.models) / 2
-            df = df.withColumn(self.getPredictionCol(), (sum_predictions > majority_threshold).cast("double"))
+            majority_vote = F.expr(f"array_position({self.getRawPredictionCol()}, array_max({self.getRawPredictionCol()})) - 1")  # maybe just mode from array
+            df = df.withColumn(self.getPredictionCol(), majority_vote.cast("double"))
         elif mode == "attack": # At least one positive vote; then positive
             positive_vote = F.expr(f"array_contains({predictions_col}, 1)")
             df = df.withColumn(self.getPredictionCol(), positive_vote.cast("double"))
