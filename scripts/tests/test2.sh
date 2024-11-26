e4a26a9ed24dbe15f0b8556e1099115adde858e6
vmsou <viniciusmarcs@icloud.com>
2024-11-26 14:32:02 -0300

    update: Test 2 - submit conf (spark.storage.memoryFraction)

--- scripts/tests/test2.sh
+++ scripts/tests/test2.sh
@@ -7,11 +7,9 @@ fi
 # Combine all arguments into the models list
 models=$(printf "%s " "$@" | sed 's/ $//')
 datasets="datasets/NF-UNSW-NB15-v2.parquet datasets/NF-ToN-IoT-v2.parquet datasets/NF-CSE-CIC-IDS2018-v2.parquet datasets/NF-BoT-IoT-v2.parquet"
-
-base_command="spark-submit --executor-memory 4g apps/evaluate-model.py -d $datasets -m $models --ensemble %s --log metrics/%s_ensemble.csv"
-
-
+base_command="spark-submit --conf spark.storage.memoryFraction=0.3 --executor-memory 4g apps/evaluate-model.py -d $datasets -m $models --ensemble %s --log metrics/%s_ensemble.csv"
 ensemble_types=("majority" "attack" "normal")
+
 for ensemble_type in "${ensemble_types[@]}"; do  
     command=$(printf "$base_command" "$ensemble_type" "$ensemble_type")
     eval "$command"
