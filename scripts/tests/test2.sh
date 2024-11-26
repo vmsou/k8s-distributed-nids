10cd7c7ace14c46992d9736a08da5f2d6d200243
vmsou <viniciusmarcs@icloud.com>
2024-11-25 22:45:03 -0300

    update: Test 2 - Models as arguments

--- scripts/tests/test2.sh
+++ scripts/tests/test2.sh
@@ -1,7 +1,18 @@
-models="models/DTC_3F_PRAUC_NB15.model models/GBT_3F_PRAUC_ToN.model models/RF_3F_PRAUC_IDS.model"
+# Ensure at least one model is provided
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <model1> [model2] [model3] ..."
+  exit 1
+fi
+
+# Combine all arguments into the models list
+models=$(printf "%s " "$@" | sed 's/ $//')
 datasets="datasets/NF-UNSW-NB15-v2.parquet datasets/NF-ToN-IoT-v2.parquet datasets/NF-CSE-CIC-IDS2018-v2.parquet datasets/NF-BoT-IoT-v2.parquet"
+
+base_command="spark-submit --executor-memory 4g apps/evaluate-model.py -d $datasets -m $models --ensemble %s --log metrics/%s_ensemble.csv"
+
 
 ensemble_types=("majority" "attack" "normal")
 for ensemble_type in "${ensemble_types[@]}"; do  
-  eval "spark-submit --executor-memory 4g apps/evaluate-model.py -d $datasets -m $models --ensemble $ensemble_type --log metrics/${ensemble_type}_ensemble.csv"
+    command=$(printf "$base_command" "$ensemble_type" "$ensemble_type")
+    eval "$command"
 done
