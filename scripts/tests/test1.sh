0af7747c7a2e08569eab591d072efdb8539f4e87
vmsou <viniciusmarcs@icloud.com>
2024-11-28 18:52:30 -0300

    update: Test Scripts - test1.sh (model name updated & metric)

--- scripts/tests/test1.sh
+++ scripts/tests/test1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FOLDS=2
+METRIC="areaUnderPR"
 dataset_names=("NB15" "ToN" "IDS" "BoT")
 datasets="datasets/NF-UNSW-NB15-v2.parquet datasets/NF-CSE-CIC-IDS2018-v2.parquet datasets/NF-ToN-IoT-v2.parquet datasets/NF-BoT-IoT-v2.parquet"
 
@@ -8,7 +9,7 @@ base_command="spark-submit --conf spark.storage.memoryFraction=0.3 --executor-me
 
 for dataset_name in "${dataset_names[@]}"; do
   # Dynamically build model paths
-  models="models/DT_${FOLDS}F_PRAUC_${dataset_name}.model models/GBT_${FOLDS}F_PRAUC_${dataset_name}.model models/RF_${FOLDS}F_PRAUC_${dataset_name}.model models/LR_${FOLDS}F_PRAUC_${dataset_name}.model models/MLP_${FOLDS}F_PRAUC_${dataset_name}.model"
+  models="models/DT_${FOLDS}F_${METRIC}_${dataset_name}.model models/GBT_${FOLDS}F_${METRIC}_${dataset_name}.model models/RF_${FOLDS}F_${METRIC}_${dataset_name}.model models/LR_${FOLDS}F_${METRIC}_${dataset_name}.model models/MLP_${FOLDS}F_${METRIC}_${dataset_name}.model"
   log_file="metrics/${dataset_name}_metrics.csv"
   eval_command=$(printf "$base_command" "$models" "$log_file")
   eval "$eval_command"
