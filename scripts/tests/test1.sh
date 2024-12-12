6bcbb8ba0ddce332536c6a44b5e4bb3f71535f42
vmsou <viniciusmarcs@icloud.com>
2024-12-12 13:34:45 -0300

    update: Scripts - test1.sh (build models path && naming)

--- scripts/tests/test1.sh
+++ scripts/tests/test1.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-FOLDS=2
-METRIC="areaUnderPR"
+FOLDS=5
+METRIC="f1"
 dataset_names=("NB15" "ToN" "IDS" "BoT")
 datasets="datasets/NF-UNSW-NB15-v2.parquet datasets/NF-CSE-CIC-IDS2018-v2.parquet datasets/NF-ToN-IoT-v2.parquet datasets/NF-BoT-IoT-v2.parquet"
 
-base_command="spark-submit --conf spark.storage.memoryFraction=0.3 --executor-memory 4g apps/evaluate-model.py -d $datasets -m %s --log %s"
+model_order=("DT" "GBT" "LR" "MLP" "RF")
+
+base_command="spark-submit --executor-memory 9g apps/evaluate-model.py -d $datasets -m %s --log %s"
 
 for dataset_name in "${dataset_names[@]}"; do
-  # Dynamically build model paths
-  models="models/DT_${FOLDS}F_${METRIC}_${dataset_name}.model models/GBT_${FOLDS}F_${METRIC}_${dataset_name}.model models/RF_${FOLDS}F_${METRIC}_${dataset_name}.model models/LR_${FOLDS}F_${METRIC}_${dataset_name}.model models/MLP_${FOLDS}F_${METRIC}_${dataset_name}.model"
+  models=""
+  for model in "${model_order[@]}"; do
+    models+="models/${model}/${model}_${FOLDS}F_${METRIC}_${dataset_name}.model "
+  done
+
   log_file="metrics/${dataset_name}_metrics.csv"
+  
   eval_command=$(printf "$base_command" "$models" "$log_file")
   eval "$eval_command"
 done
